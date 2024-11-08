1d7e4df60912a7f4cbbc13cc81535fcfd552b08c
Trey <35114142+benjaminbrown038@users.noreply.github.com>
2024-11-08 10:16:59 -0500

    Update 2-token-classification.py

--- 2-token-classification.py
+++ 2-token-classification.py
@@ -1,4 +1,6 @@
 from huggingface_hub import notebook_login
+notebook_login()
+
 from datasets import load_dataset
 from transformers import AutoTokenizer
 from transformers import DataCollatorForTokenClassification
@@ -13,3 +15,22 @@ from transformers.keras_callbacks import PushToHubCallback
 from transformers import pipeline
 from transformers import AutoModelForTokenClassification
 
+wnut = load_dataset("wnut_17")
+wnut["train"][0]
+
+label_list = wnut["train"].features[f"ner_tags"].feature.names
+label_list
+
+tokenizer = AutoTokenizer.from_pretrained("distilbert-base-uncased")
+example = wnut["train"][0]
+tokenized_input = tokenizer
+tokens
+
+def tokenize_and_align_labels():
+    return tokenized_inputs
+
+data_collator = DataCollatorForTokenClassification(tokenizer=tokenizer)
+data_collator = DataCollatorForTokenClassification(tokenizer=tokenizer, return_tensors = "tf")
+
+seqeval = eval.load("seqeval")
+lavels = 
