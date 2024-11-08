33f410795c0ff5602c0694bec0e3535fd9e26036
Trey <35114142+benjaminbrown038@users.noreply.github.com>
2024-11-08 11:11:58 -0500

    Create 6-translation.py

---
+++ 6-translation.py
@@ -0,0 +1,15 @@
+!pip install transformers datasets evaluate sacrebleu
+
+from huggingface_hub import notebook_login
+notebook_login()
+from datasets import load_dataset
+from transformers import AutoTokenizer
+from transformers import DataCollatorForSeq2Seq
+import evaluate
+import numpy as np
+from transformers import AutoModelForSeq2SeqLM, Seq2SeqTrainingArguments, Seq2SeqTrainer
+from transformers import AdamWeightDecay
+import tensorflow as tf
+from transformers.keras_callbacks import KerasMetricCallback
+from transformers.keras_callbacks import PushToHubCallback
+from transformers import pipeline
