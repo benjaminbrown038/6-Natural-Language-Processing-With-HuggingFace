bf26a5c67e695a5897dfb506e30a8e6cbde83ad6
Trey <35114142+benjaminbrown038@users.noreply.github.com>
2024-11-07 20:40:22 -0500

    Update 1-text-classification.py

--- 1-text-classification.py
+++ 1-text-classification.py
@@ -1,38 +1,77 @@
 !pip install transformers datasets evaluate accelerate
 
 from huggingface_hub import notebook_login
+from datasets import load_dataset
+from transformers import AutoTokenizer
+from transformers import DataCollatorWithPadding
+from transformers import AutoModelForSequenceClassification, TrainingArguments, Trainer
+from transformers import create_optimizer
+from transformers import TFAutoModelForSequenceClassification
+import evaluate
+import numpy as np
+
+
+
+
 
 notebook_login()
 
-from datasets import load_dataset
 
 imbd = load_dataset("imbd")
-
 imbd["test"][0]
-
-from transformers import AutoTokenizer
 tokenizer = AutoTokenizer.from_pretrained("distilbert/distilbert-base-uncased")
 
 def preprocess_function(examples):
     return tokenizer(examples["text"], truncation=True)
 
 tokenized_imdb = imdb.map(preprocess_function, batched=True)
-
-from transformers import DataCollatorWithPadding
-
 data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
-
-from transformers import DataCollatorWithPadding
-
 data_collator = DataCollatorWithPadding(tokenizer=tokenizer, return_tensors="tf")
-
-import evaluate
-
 accuracy = evaluate.load("accuracy")
 
-import numpy as np
 
 def compute_metrics(eval_pred):
     predictions, labels = eval_pred
     predictions = np.argmax(predictions,axis=1)
-    return accuracy.compute(predictions = 
+    return accuracy.compute(predictions = predictions, references=labels)
+
+id2label = {0: "NEGATIVE", 1: "POSITIVE"}
+label2id = {"NEGATIVE": 0, "POSITIVE": 1}
+
+model = AutoModelForSequenceClassification.from_pretrained("distilbert/distilbert-base-uncased", num_labels=2, id2label=id2label, label2id=label2id)
+
+training_args = TrainingArguments(
+    output_dir="my_awesome_model",
+    learning_rate=2e-5,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
+    num_train_epochs=2,
+    weight_decay=0.01,
+    eval_strategy="epoch",
+    save_strategy="epoch",
+    load_best_model_at_end=True,
+    push_to_hub=True,
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_imdb["train"],
+    eval_dataset=tokenized_imdb["test"],
+    processing_class=tokenizer,
+    data_collator=data_collator,
+    compute_metrics=compute_metrics,
+)
+
+trainer.train()
+
+trainer.push_to_hub()
+
+from transformers import create_optimizer
+import tensorflow as tf
+
+batch_size = 16
+num_epochs = 5
+batches_per_epoch = len(tokenized_imdb["train"]) // batch_size
+total_train_steps = int(batches_per_epoch * num_epochs)
+optimizer, schedule = create_optimizer(init_lr=2e-5, num_warmup_steps=0, num_train_steps=total_train_steps)
